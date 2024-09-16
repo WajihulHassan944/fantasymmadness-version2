aa54e8abb162084ae2782a01e67738d74dfeedd1
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2024-09-16 16:55:57 +0500

    updated style

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -9,16 +9,18 @@ const Home = () => {
     <h2 data-aos="zoom-out">Boxing, MMA,<br /> Kickboxing & BK</h2>
 
     <div class="video-embed-wrapper" >
-    <iframe 
-        width="560" 
-        height="315" 
-        src="https://www.youtube.com/embed/C5wHWEzPrrs?autoplay=1&loop=1&playlist=C5wHWEzPrrs" 
-        title="YouTube video player" 
-        frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        allowfullscreen >
-    </iframe>
-    
+    <div className="no-hover">
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/C5wHWEzPrrs?autoplay=1&loop=1&playlist=C5wHWEzPrrs&controls=0&modestbranding=1&rel=0&fs=0"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen
+  ></iframe>
+</div>
+
 </div> 
     </div>
 
