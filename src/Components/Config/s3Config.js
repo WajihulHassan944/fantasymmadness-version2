3836522c0fa2bf4a4363a9046cc613e266ea87b8
WajihulHassan944 <82878378+WajihulHassan944@users.noreply.github.com>
2024-10-31 15:25:36 +0500

    updated s3

--- src/Components/Config/s3Config.js
+++ src/Components/Config/s3Config.js
@@ -1,12 +1,11 @@
-// src/config/s3Config.js
 import AWS from 'aws-sdk';
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
   region: 'us-east-1',  // US East (N. Virginia)
 });
-console.log("Access Key:", process.env.AWS_ACCESS_KEY_ID); // For debugging only
+console.log("Access Key:", process.env.REACT_APP_AWS_ACCESS_KEY_ID); // For debugging only
 
 const s3 = new AWS.S3();
 export default s3;
